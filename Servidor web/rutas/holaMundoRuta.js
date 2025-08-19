9e304f2a7562801707ba8f26028e6cc510a8ad14
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-18 22:34:31 -0700

    Ruta Hola Mundo - Servidor WEB
    Ruta para mensaje get con el clásico Hola Mundo.

---
+++ Servidor web/rutas/holaMundoRuta.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+router
+    .route('/')
+    .get((req, res) => {
+        res.send('¡Hola, mundo!')
+    })
+
+
+module.exports = router;
+
