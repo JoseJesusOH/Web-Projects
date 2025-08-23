0d1cc01f52f3abf57203790cc14647bb071bceed
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-22 21:28:32 -0700

    Servicio Eliminar Cliente - Soldadura
    Servicio para eliminar un cliente del sistema en base al id, y a su vez devolver el mensaje correspondiente.

--- EmergenteSoldadura/servicios/servicioClientes.js
+++ EmergenteSoldadura/servicios/servicioClientes.js
@@ -31,3 +31,23 @@ exports.obtenerClientes = asyncError(async (req, res, next) => {
         });
     }
 });
+
+exports.eliminarCliente = asyncError(async (req, res, next) => {
+    const result = await controlClientes.obtenerClientePorId(req.params.id);
+    if (typeof result === 'string') {
+        const error = new CustomeError('No se encontró el cliente', 404);
+        return next(error);
+    }
+    const result2 = await controlClientes.eliminarCliente(req.params.id);
+    if (typeof result2 === 'string') {
+        const error = new CustomeError('Error al eliminar el cliente', 400);
+        return next(error);
+    } else {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                cliente: result
+            }
+        });
+    }
+});
