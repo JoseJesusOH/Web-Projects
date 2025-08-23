72d6552ae293de8ee74589e2a8c15a737a561b99
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-22 21:29:28 -0700

    Servicio Actualizar Cliente - Soldadura
    Servicio para actualizar un cliente donde el cuerpo son los datos a actualizar del cliente modelados previamente.

--- EmergenteSoldadura/servicios/servicioClientes.js
+++ EmergenteSoldadura/servicios/servicioClientes.js
@@ -51,3 +51,23 @@ exports.eliminarCliente = asyncError(async (req, res, next) => {
         });
     }
 });
+
+exports.actualizarCliente = asyncError(async (req, res, next) => {
+    const result = await controlClientes.obtenerClientePorId(req.params.id);
+    if (typeof result === 'string') {
+        const error = new CustomeError('Error al obtener el cliente', 404);
+        return next(error);
+    }
+    const result2 = await controlClientes.actualizarCliente(req.body);
+    if (typeof result2 === 'string') {
+        const error = new CustomeError('Error al actualizar el cliente', 400);
+        return next(error);
+    } else {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                cliente: req.body
+            }
+        });
+    }
+});
