6cfbe3a8c4f9362f0e501aa8167679e86cdb2987
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-23 18:58:27 -0700

    Rutas Agregar, Obtener Retroalimentaciones - Soldadura
    Rutas para los servicios de agregar u obtener retroalimentaciones del sistema.

---
+++ EmergenteSoldadura/rutas/retroalimentacionesRutas.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const servicioRetroalimentaciones = require('../servicios/servicioRetroalimentaciones');
+
+const router = express.Router();
+
+
+router
+.route('/')
+.get(servicioRetroalimentaciones.obtenerRetroalimentaciones)
+.post(servicioRetroalimentaciones.agregarRetroalimentacion)
+
+
+module.exports = router;
