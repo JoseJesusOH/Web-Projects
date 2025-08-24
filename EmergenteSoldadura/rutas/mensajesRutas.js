0b672edc42cc7c44ffa7c573b1f33d032009c0bb
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-23 18:54:14 -0700

    Rutas Agregar, Obtener Mensaje - Soldadura
    Rutas para los servicios de agregar o obtener mensajes del sistema o hacia el mismo.

---
+++ EmergenteSoldadura/rutas/mensajesRutas.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const servicioMensajes = require('../servicios/servicioMensajes');
+
+const router = express.Router();
+
+
+router
+.route('/')
+.get(servicioMensajes.obtenerMensajes)
+.post(servicioMensajes.agregarMensaje)
+
+
+
+module.exports = router;
